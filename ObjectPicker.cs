4ba8e50b27c5f6765714e2aaf3e942237d52d019
squidypal <squidypal@outlook.com>
2023-11-30 17:30:29 -0800

    Tool tips + comments

--- ObjectPicker.cs
+++ ObjectPicker.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class ObjectPicker : MonoBehaviour
 {
+    [Tooltip("The speed at which the object will follow the mouse / center of the screen")]
     public float followSpeed = 3f;
+    [Tooltip("The speed at which the object will move closer / further away from the camera")]
     public float scrollSpeed = 5f;
     private GameObject selectedObject;
     private Rigidbody selectedRigidbody;
     private Vector3 targetPosition;
     private float distanceToCamera;
+    // You can add a constant force if you want the object to spin while being held
     private ConstantForce constantForce;
+    [Tooltip("If true, the player can use the scroll wheel to bring objects closer / further away from the camera")]
     public bool scrollEnabled = true;
+    [Tooltip("The max distance the raycast can travel")]
     public int maxDistance = 20;
     
+    // Returns true if the player is holding the specified object, or if anyObject is true, if the player is holding any object
     public bool isHoldingObject(GameObject obj, bool anyObject)
     {
         if (anyObject)
